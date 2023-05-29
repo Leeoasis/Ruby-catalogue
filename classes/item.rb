3d97ae55d885b96c841771c351c63edec2cc722a
leslie <Kooliesk@gmail.com>
2023-05-29 15:48:03 +0200

    Add and implement item class

---
+++ classes/item.rb
@@ -0,0 +1,26 @@
+require 'date'
+
+class Item
+
+    def initialize(genre, author, label, publish_date, archived)
+        @genre = genre
+        @author = author
+        @label = label
+        @publish_date = publish_date
+        @archived = archived
+    end
+    
+    def can_be_archived?
+        today = Date.today
+        ten_years_ago = (today - 10 * 365)
+        @publish_date < ten_years_ago
+    end
+
+    def move_to_archive
+         @archived = true if can_be_archived?
+    end
+
+end
+
+new_item = Item.new("Fantasy", "J.K. Rowling", "Bloomsbury", Date.new(2014,5,30), false)
+puts(new_item.move_to_archive)
