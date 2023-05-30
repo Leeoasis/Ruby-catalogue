ce52bdbf6001d9dc8c0d60e2500ea44d6348cd2b
Leslie Gudo <Kooliesk@gmail.com>
2023-05-30 15:44:32 +0200

    Update item.rb

--- classes/item.rb
+++ classes/item.rb
@@ -4,19 +4,20 @@ class Item
   attr_accessor :genre, :author, :label, :archived
   attr_reader :id, :publish_date
 
-  def initialize(publish_date, archived)
+  def initialize(publish_date)
     @id = rand(1..1000)
     @publish_date = publish_date
     @archived = archived
   end
 
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+  
+  private
   def can_be_archived?
     today = Date.today
     ten_years_ago = (today - (10 * 365))
     @publish_date < ten_years_ago
-  end
-
-  def move_to_archive
-    @archived = true if can_be_archived?
   end
 end
