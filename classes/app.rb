a00b609e810890b4faa76f7180c7c97727f93463
Amine derbal <aminedoran31@gmail.com>
2023-05-30 17:29:21 +0100

    add load and save methods

--- classes/app.rb
+++ classes/app.rb
@@ -1,15 +1,19 @@
 require_relative '../modules/display_menu_options'
 require_relative '../modules/music_album_module'
+require_relative '../modules/utils'
+require_relative '../classes/collections'
 
 class App
   include DisplayMenuOptions
   include MusicAlbumModule
+  include Utils
 
   def initialize
+    check_data_folder
     @books = []
-    @music_albums = []
+    @music_albums = Collections.load_data('music_albums').empty? ? [] : Collections.load_data('music_albums')
     @games = []
-    @genres = []
+    @genres = Collections.load_data('genres').empty? ? [] : Collections.load_data('genres')
   end
 
   def run
@@ -27,11 +31,11 @@ class App
     when 1
       list_books
     when 2
-      list_music_albums
+     list_music_albums
     when 3
       list_games
     when 4
-      list_genres
+     list_genres
     when 5
       list_label
     when 6
@@ -40,6 +44,8 @@ class App
       add_book
     when 8
       add_music_album
+     Collections.save_data('music_albums', @music_albums)unless @music_albums.empty?
+     Collections.save_data('genres', @genres) unless @genres.empty?
     when 9
       add_game
     else
