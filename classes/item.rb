5d63e3b8037cd700deedb52ed9d8b383f4c1951c
Amine derbal <aminedoran31@gmail.com>
2023-06-01 13:44:45 +0100

    Add setters methods for genre author and label

--- classes/item.rb
+++ classes/item.rb
@@ -14,6 +14,21 @@ class Item
     @archived = true if can_be_archived?
   end
 
+  def add_genre(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.add_item(self) unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
+  end
+
   private
 
   def can_be_archived?
